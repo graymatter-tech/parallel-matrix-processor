86b913f5a5782d26b837d1c495bee6aad76b26ee
matthew <mattgray1094@gmail.com>
2019-09-29 22:12:11 +1000

    Clean up and documentation

--- matrix_processor.c
+++ matrix_processor.c
@@ -1,7 +1,3 @@
-/* Matrix processing in mpi . 
- * Elements are replaced by the average of their neighbours 
- * 
- */
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -10,22 +6,45 @@
 #include <fcntl.h>
 #include "mpi.h"
 #include "matrix.h"
+
 #define MainProcess 0
 #define UseRows 2
+
+/**
+ * MPI Matrix Processor
+ * ---------------------
+ * Author: Matthew Gray
+ * Student Num: 220186070
+ * Email: mgray44@myune.edu.au
+ *  
+ * This program reads in a square matrix of any dimension
+ * and replaces each element in that matrix with the average of its neighbours.
+ * 
+ * NOTE
+ * ------
+ * The current version of this program doesn't manage to reach the assignment 
+ * specs. It can only manage to split the matrix into an even number of rows per
+ * process. Futhermore it can only handle a neighbourhood depth of 1.
+ * All my attempts to extend the functionality of the program ended up breaking it
+ * and sadly I needed to submit.
+ * 
+ * Compile:
+ * make matrix_processor
+ * 
+ * Run:
+ * mpirun -np NumProcessors matrix_processor InputMatrix OutputMatrix Matrix Dimension
+ * NumProcesses: Number of processes that divide up the matrix
+ * InputMatrix: Name of Input Matrix
+ * OutputMatrix: Name of matrix solution is output to
+ * Dimension: Dimension of input (and thus output) matrix
+ * 
+ */
 
 // Number of rows each process calculates
 int rows;
 // Number of extra rows the first row gets (if the split isn't even)
 int frow = 0;
 
-/**
- * TO DO
- * -----
- * - Implement Way to Send multiple rows to a single process
- * - See if you can determine a matrix's size/dimension without being passed it?
- * - Document!
- * 
- */
 
 int parse_args (int argc, char *argv[], int *fd, int *np)
 {
@@ -61,17 +80,10 @@ int main (int argc, char *argv[])
       }
     }
 
-  /* broadcaste dim to all */
+  // Broadcast dim to all
   MPI_Bcast (&dim, 1, MPI_INT, MainProcess, MPI_COMM_WORLD);
 
-  // if (dim != nprocs) {
-  //     if (me == MainProcess)
-  //       fprintf (stderr,
-  //       "Usage: mpirun -np dimension %s matrixA matrixB dimension\n", argv[0]);
-  //     MPI_Finalize ();
-  //     exit (EXIT_FAILURE);
-  // }
-
+  // Determine how many rows of the matrix each process gets
   if (dim == nprocs || nprocs > dim) {
     rows = 1;
   } else if (dim%nprocs == 0) {
@@ -81,22 +93,23 @@ int main (int argc, char *argv[])
     frow = dim%nprocs;
   }
 
+  // Total number of rows required by each process
   int totalRows = UseRows + rows;
 
+  // Declare each required matrix
   int A[dim + 1][dim + 1], P[dim + 1][dim + 1], superA[dim + 2][dim + 2];
   int Arow[totalRows][dim + 2], Prow[rows][dim + 1];
-  //int *Prow = (int *)malloc(rows*(dim + 2)*sizeof(int));
 
   if (me == MainProcess) {				
-    /* parent code */
-    /* initialize A */
+    // Parent Code
+    // Initialise A
     for (i = 1; i < dim + 1; i++)
       if (get_row (fd[0], dim, i, &A[i][1]) == -1) {
           fprintf (stderr, "Initialization of A failed\n");
           goto fail;
       }
 
-    /* initialize supersized matrix */
+    // Initialise the supersized matrix
     for (row = 0; row < dim + 2; row++)
       superA[row][0] = superA[row][dim + 1] = 0;
     for (col = 0; col < dim + 2; col++)
@@ -106,9 +119,12 @@ int main (int argc, char *argv[])
         superA[row][col] = A[row][col];
   }
 
+  int *displs;q
+  displs = (int *)malloc(nprocs*)
+
   
   
-  /* Scatter super sized A */
+  // Scatted the supersized matrix
   for (i = 0; i < totalRows; i++) {
     if (MPI_Scatter (&superA[i],
 		     (dim + 2)*rows,
@@ -122,34 +138,33 @@ int main (int argc, char *argv[])
       }
   }
   
+  // Counter for non-zero neighbours
   int count = -1;
   
-  
-  
+  // Any process with no rows assigned, skip over processing
   if (me < dim) {
+    // Iterate through each rows the process calculates
     for (r = 1; r < rows + 1; r++) {
-    for (i = 1; i < dim + 1; i++) {
-      Prow[r][i] = -Arow[r][i];
-      for (j = r-1; j < UseRows + r; j++) {
-        printf("%d %d %d\n", Arow[j][i - 1], Arow[j][i], Arow[j][i + 1] );
-        if (Arow[j][i - 1] != 0) count++;
-        if (Arow[j][i] != 0) count++;
-        if (Arow[j][i + 1] != 0) count++;
-        Prow[r][i] += Arow[j][i - 1] + Arow[j][i] + Arow[j][i + 1];
+      // For each element in that row
+      for (i = 1; i < dim + 1; i++) {
+        // Counteract the effect of summing itself
+        Prow[r][i] = -Arow[r][i];
+        for (j = r-1; j < UseRows + r; j++) {
+          if (Arow[j][i - 1] != 0) count++;
+          if (Arow[j][i] != 0) count++;
+          if (Arow[j][i + 1] != 0) count++;
+          Prow[r][i] += Arow[j][i - 1] + Arow[j][i] + Arow[j][i + 1];
+        }
+        // Average out the elements by its nonzero neighbours
+        Prow[r][i] /= count;
+        count = -1;
       }
-      printf("%d\n",count);
-      Prow[r][i] /= count;
-      count = -1;
-      printf("Process %d: Element: %d\n", me , Prow[r][i]);
-    }
-    printf("New row!\n");
-    printf("r = %d\n",r);
     }
 
   }
     
   
-  /* Gather rows of P */
+  // Gather rows of output Matrix
   if (MPI_Gather (&Prow[1][0],
       (dim+1)*rows,
       MPI_INT,
@@ -160,19 +175,8 @@ int main (int argc, char *argv[])
       fprintf (stderr, "Gathering of Product  failed\n");
       goto fail;
     }
-    // if (MPI_Gather (&Prow[0],
-    // 	  dim + 1,
-    // 	  MPI_INT,
-    // 	  &P[1][0],
-    // 	  dim + 1,
-    // 	  MPI_INT, MainProcess, MPI_COMM_WORLD) != MPI_SUCCESS)
-    //   {
-    //     fprintf (stderr, "Gathering of Product  failed\n");
-    //     goto fail;
-    //   }
 
-
-  /* write the matrix to a file */
+  // Write the matrix to output file
   if (me == MainProcess) {
     for (i = 1; i < dim + 1; i++)
       if (set_row (fd[1], dim, i, &P[i][1]) == -1)
